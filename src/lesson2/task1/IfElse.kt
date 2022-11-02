b3ad9f4f294d9a02eda2f7d752932b542ac45fc1
4842394 <tshur.la@edu.spbstu.ru>
2022-11-02 14:15:39 +0300

    triangleKind is done

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -141,13 +141,14 @@ fun triangleKind(a: Double, b: Double, c: Double): Int {
     val maximumSide = maxOf(a, b, c)
     val minimumSide = minOf(a, b, c)
     val middleSide = a + b + c - maximumSide - minimumSide
-    if ((middleSide + minimumSide > maximumSide) and (middleSide + maximumSide > minimumSide)
-        and (maximumSide + minimumSide > middleSide)) {
-        if (maximumSide.pow(2.0) < middleSide.pow(2.0) + minimumSide.pow(2.0)) return 0
-        if (maximumSide.pow(2.0) == middleSide.pow(2.0) + minimumSide.pow(2.0)) return 1
-        if (maximumSide.pow(2.0) > middleSide.pow(2.0) + minimumSide.pow(2.0)) return 2
-    }
-    return -1
+    if ((middleSide + minimumSide > maximumSide) && (middleSide + maximumSide > minimumSide)
+        && (maximumSide + minimumSide > middleSide)) {
+        when {
+            (maximumSide.pow(2.0) < middleSide.pow(2.0) + minimumSide.pow(2.0)) -> return 0
+            (maximumSide.pow(2.0) == middleSide.pow(2.0) + minimumSide.pow(2.0)) -> return 1
+            else -> return 2
+        }
+    } else return -1
 }
 
 
